dd71721c3f0b5094b187b311a57c78ac79780f93
Sanidhya Patidar <123492565+sanidhya1411@users.noreply.github.com>
2024-03-16 14:28:19 +0530

    vercel

---
+++ routes/userRoutes.js
@@ -0,0 +1,18 @@
+const { Router } = require('express')
+
+const { registerUser, loginUser, getUser, editUser, getAuthors, changeAvatar,ResetPassword,ForgotPassword } = require('../controllers/userControllers')
+const authMiddleware = require('../middleware/authMiddleware')
+
+const router = Router()
+
+
+router.post('/register', registerUser)
+router.post('/login', loginUser)
+router.get('/:id', getUser)
+router.get('/', getAuthors)
+router.post('/change-avatar',authMiddleware,changeAvatar)
+router.patch('/edit-user', authMiddleware, editUser)
+router.post('/forgotPassword',ForgotPassword)
+router.patch('/resetPassword/:token',ResetPassword)
+
+module.exports = router
